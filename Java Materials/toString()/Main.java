2b1713f003bd86556b7e36677ea07214496c05dd
hananshafay72442 <hananshafay72442@gmail.com>
2026-04-11 21:19:28 +0500

    Add files via upload

---
+++ Java Materials/toString()/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("Ford", "Mustang", 2025, "Red");
+
+        System.out.println(car);
+        // System.out.printf("%s %d %s %s", car.color, car.year, car.make, car.model);
+    }
+}
