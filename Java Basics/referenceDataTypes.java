2f79a2cb0cecd789f3032aa56f4b76abe9bdc115
hananshafay72442 <hananshafay72442@gmail.com>
2026-04-11 21:16:57 +0500

    Delete Java Basics directory

--- Java Basics/referenceDataTypes.java
+++
@@ -1,6 +0,0 @@
-public class referenceDataTypes {
-    public static void main(String[] args) {
-        String name = "Muhammad Hanan Shafay Shah";
-        System.out.println("Hello! " + name);
-    }
-}
